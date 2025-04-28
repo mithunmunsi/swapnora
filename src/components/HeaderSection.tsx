a60eb7e4ee459147d32cbccb04d3c436702e64af
Mithun Munsi <mithun.munsi@gmail.com>
2025-04-29 02:10:13 +0300

    Open graph content updates

--- src/components/HeaderSection.tsx
+++ src/components/HeaderSection.tsx
@@ -5,7 +5,7 @@ const HeaderSection = () => {
     <div className="header-container">
       <h1 className="header-title">Where Small Gifts Build Big Tomorrows.</h1>
       <h2 className="header-subtitle">Swapnora Fundraiser</h2>
-      <p className="header-author">by Nextby</p>
+      <p className="header-author">Operated by Nextby</p>
     </div>
   );
 };
